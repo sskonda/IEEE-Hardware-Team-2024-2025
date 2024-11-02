b59c710a22b15a339beddad51b2a2740f9829165
ASattar04 <sattarahmed204@gmail.com>
2024-11-02 19:12:24 -0400

    updated april tag reading

--- Camera_Code/main.py
+++ Camera_Code/main.py
@@ -1,3 +1,26 @@
-from ..readAprilTag.py import readTag
+from pi_camera.read_april_tag import read_april_tag
+from pi_camera.read_camera import capture_image_from_picam2
+
+def main(): 
+
+    imgDir = "pi_camera/read_camera/images/testIMG.png"
+
+    capture_image_from_picam2.read_Cam(imgDir)
+
+    tags = read_april_tag.readTag(imgDir)
+
+    if tags:
+        for tag in tags:
+            print("Tag ID: ", tag.tag_id)
+            print("Tag Corners: ", tag.corners)
+
+    else:
+        print("No AprilTag detected.")
+
+
+
+if __name__ == "main":
+    main()
+
 
 
--- Camera_Code/pi_camera/read_camera/capture_image_from_picam2.py
+++ Camera_Code/pi_camera/read_camera/capture_image_from_picam2.py
@@ -1,8 +1,10 @@
 from picamera import PiCamera
-
 from time import sleep
-camera = PiCamera()
-camera.start_preview(alpha= 192)
-sleep(1)
-camera.capture("home/pi/Desktop/pic.jpg")
-camera.stop-preview()
+
+def read_Cam(file):
+
+    camera = PiCamera()
+    camera.start_preview(alpha= 192)
+    sleep(1)
+    camera.capture(file)
+    camera.stop-preview()

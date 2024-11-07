5c39d11d112d7615f81d8b1c6a7aac1989830d4c
ASattar04 <sattarahmed204@gmail.com>
2024-11-06 20:35:10 -0500

    fixed files

--- Camera_Code/Capture_image_from_PiCam2.py
+++
@@ -1,8 +0,0 @@
-from picamera import PiCamera
-
-from time import sleep
-camera = PiCamera()
-camera.start_preview(alpha= 192)
-sleep(1)
-camera.capture("python-apriltag/python-apriltag/images/testIMG.jpg")
-camera.stop-preview()

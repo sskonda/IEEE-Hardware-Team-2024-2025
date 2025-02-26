66a20ab05441e65f8996ce2133c871011ca1b77f
Nikhil Iyer <iyer.nikhil@ufl.edu>
2025-02-26 01:45:26 -0500

    Read IMU!

--- read_imu.py
+++ read_imu.py
@@ -1,17 +1,20 @@
 #!/bin/python3
 
+import numpy as np
 import pigpio
 import time
 import struct
 
+import sys
 
-buffer = memoryview
+calibrate = len(sys.argv) > 1 and sys.argv[1] == '--calibrate'
+num_samples = 1000000
 
 BUS=1
 
-MPU_6050_I2C_ADDR=0b1101001
+MPU_6050_I2C_ADDR=0x68
 
-RUNTIME=60.0
+RUNTIME=300.0
 
 # Registers.
 
@@ -34,27 +37,40 @@ MPU_6050_GYRO_ZOUT_L = 0x48 # R
 pi=pigpio.pi() # open local Pi
 
 h = pi.i2c_open(BUS, MPU_6050_I2C_ADDR)
-read = 0
 
 if h >= 0: # Connected OK?
+   read = 0
+   accel_sum = np.array([0, 0, 0])
+   gyro_sum = np.array([0, 0, 0])
+
+   calibration_data = np.empty((num_samples, 6))
+
    # Initialise MPU_6050.
    start_time = time.time()
+   print(f"Who Am I {pi.i2c_read_byte_data(h, MPU_6050_WHO_AM_I)}")
 
-   while (time.time()-start_time) < RUNTIME:
+   while (time.time()-start_time) < RUNTIME and (read < num_samples or not calibrate):
 
       # > = big endian
-      print(f"Who Am I {pi.i2c_read_byte_data(h, MPU_6050_WHO_AM_I)}")
-
-      (s, b) = pi.i2c_read_i2c_block_data(h, MPU_6050_TEMP_OUT_H, 6 + 2 + 6)
+      (s, b) = pi.i2c_read_i2c_block_data(h, MPU_6050_ACCEL_XOUT_H, 6 + 2 + 6)
 
       if s >= 0:
-         (accel_x, accel_y, accel_z, t, gyro_x, gyro_y, gyro_z) = struct.unpack('>7h', buffer(b))
-         t = 35 + ((t + 13200) / 280.0)
-         print("{:.1f} {} {} {}".format(t, gyro_x, gyro_y, gyro_z))
+         data = struct.unpack('>3hxx3h', b)
+
+         calibration_data[read] = data
+         # print(f"Acceleration: {calibration_data[read, :3]}")
+         # print(f"Gyro: {calibration_data[read, 3:]}")
          read += 1
+
+   calib_mean = np.mean(calibration_data, axis=0)
+   print(calib_mean[:3])
+   print(calib_mean[3:])
+
+   if calibrate:
+      np.save("imu_calib", calibration_data)
+   print(read, read/RUNTIME)
 
    pi.i2c_close(h)
 
 pi.stop()
 
-print(read, read/RUNTIME)

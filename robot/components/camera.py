b84bdb4c9cb3d4ccfbfdf51fb115635d5e27bc0c
Nikhil Iyer <iyer.h.nikhil@ufl.edu>
2025-03-14 21:48:26 +0000

    Recalibrate camera

--- robot/components/camera.py
+++ robot/components/camera.py
@@ -1,7 +1,8 @@
 import picamera2
-import robot
+from ..constants import *
 from . import Component
 
+from libcamera import Transform
 from apriltag import apriltag
 import cv2
 import numpy as np
@@ -9,31 +10,13 @@ import picamera2
 import sys
 import math
 
-APRILTAG_POSE = {  # inches, 0,0 top left corner, 0 degrees is east, world space
-    0: (np.array([0.0, 22.5, 2.0]), 0.0),
-    1: (np.array([0.0, 22.5, 2.0]), 0.0),
-    2: (np.array([0.0, 22.5, 2.0]), 0.0),
-    3: (np.array([0.0, 22.5, 2.0]), 0.0),
-    4: (np.array([0.0, 22.5, 2.0]), 0.0),
-    5: (np.array([32.0, 45.0, 2.0]), 270.0),
-    6: (np.array([44.0, 0.0, 2.0]), 90.0),
-    7: (np.array([93.0, 22.5, 2.0]), 180.0)
-}
-
-TAG_WIDTH = 3.125
-TAG_POINTS = np.array([  # object points of the april tag
-    [0.0, -TAG_WIDTH / 2, -TAG_WIDTH / 2,],
-    [0.0,  TAG_WIDTH / 2, -TAG_WIDTH / 2,],
-    [0.0,  TAG_WIDTH / 2,  TAG_WIDTH / 2,],
-    [0.0, -TAG_WIDTH / 2,  TAG_WIDTH / 2,]
-], dtype=np.float32)
-
 class Camera(Component):
     def __init__(self, camera: int = 0):
         super().__init__()
 
         self.camera_idx = camera
         self.detector = apriltag("tag36h11")
+        self.raw_frame = None
         self.frame = None
         self.frame_tick = None
         self.undistorted_frame = None
@@ -44,8 +27,15 @@ class Camera(Component):
         try:
             self.load_calibration()
             self.picam2 = picamera2.Picamera2(self.camera_idx)
+            
+            transform = CAMERA_CONFIGURATION.pop("transform", None)
+            if transform is not None:
+                CAMERA_CONFIGURATION["transform"] = Transform(**transform)
+
             config = self.picam2.create_video_configuration(
-                {'format': 'RGB888'})
+                {'format': 'RGB888'},
+                **CAMERA_CONFIGURATION
+            )
             self.picam2.configure(config)
             self.picam2.start()
             return True
@@ -74,41 +64,39 @@ class Camera(Component):
 
     def get_frame(self):
         frame_buffer = self.picam2.capture_array()
-        if self.frame is None:
-            self.frame = np.empty_like(frame_buffer)
+        if self.raw_frame is None:
+            self.raw_frame = np.empty_like(frame_buffer)
 
         # copy frame buffer to self.frame
-        self.frame[:, :, :] = frame_buffer[:, :, :]
+        self.raw_frame[:, :, :] = frame_buffer[:, :, :]
         self.frame_tick = self.pi.get_current_tick() if self.pi else None
         # if calibration data is available, undistort the frame
         # if self.camera_matrix is not None and self.distortion_coeffs is not None:
         #     frame = self.undistort_frame(frame)
-        return self.frame
+        if self.frame is None:
+            self.frame = np.empty_like(self.raw_frame)
+        if np.all(self.frame.shape == self.raw_frame.shape):
+            self.frame[:, :, :] = self.raw_frame[:, :, :]
+        return self.raw_frame
 
     def undistort_frame(self):
-        if self.frame is None:
-            self.undistorted_frame = None
-        else:
-            h, w = self.frame.shape[:2]
+        if self.raw_frame is not None:
+            h, w = self.raw_frame.shape[:2]
             new_camera_matrix, roi = cv2.getOptimalNewCameraMatrix(
                 # uses calibration data to get new camera matrix
                 self.camera_matrix, self.distortion_coeffs, (w, h), 1, (w, h)
             )
             undistorted_frame = cv2.undistort(
-                self.frame, self.camera_matrix, self.distortion_coeffs, None, new_camera_matrix)
+                self.raw_frame, self.camera_matrix, self.distortion_coeffs, None, new_camera_matrix)
             x, y, w, h = roi
-            self.undistorted_frame = undistorted_frame[y:y + h, x:x + w]
-        return self.undistorted_frame
+            self.frame = undistorted_frame[y:y + h, x:x + w]
+        return self.frame
 
     def detect_apriltags(self):
-        if self.undistorted_frame is not None:
-            frame = self.undistorted_frame
-        elif self.frame is not None:
-            frame = self.frame
-        else:
+        if self.frame is None:
             return None
 
-        im = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        im = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
         # DONT DELETE THIS LINE, NECESSARY FOR SOME REASON???
         sys.stdout.write("\n")
 
@@ -130,7 +118,7 @@ class Camera(Component):
             while True:
                 im = self.get_frame()
                 brightness = np.mean(cv2.cvtColor(im, cv2.COLOR_BGR2GRAY))
-                if brightness > mean + 100.0 * std:
+                if brightness > mean + 50.0 * std:
                     break
         except KeyboardInterrupt:
             print("Manual Start.")
@@ -146,7 +134,6 @@ class Camera(Component):
     def calculate_pose_matrix(self):
         if self.detections is None:
             return None
-
 
         image_points = []
         world_points = []

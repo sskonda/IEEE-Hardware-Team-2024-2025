74f03ab6e3d23f04ea6c1c7a1beaf931352a57a7
Nikhil Iyer <iyer.nikhil@ufl.edu>
2025-03-28 01:56:30 -0400

    Update pose estimation

--- calibrate_imu.py
+++ calibrate_imu.py
@@ -5,65 +5,107 @@ import pigpio
 import time
 import struct
 from tqdm import tqdm
+import numpy as np
+from tqdm import tqdm
 
-import sys
+import numpy as np
+from tqdm import tqdm
 
-num_samples = 100000
+def fit_ellipsoid(data):
+    # Data is an N x 3 numpy array of floats to fit
+    x, y, z = data[:, 0], data[:, 1], data[:, 2]
+    
+    # Construct the design matrix
+    D = np.column_stack([x**2, y**2, z**2, 2*x*y, 2*x*z, 2*y*z, 2*x, 2*y, 2*z, np.ones_like(x)])
+    
+    # Solve the least squares problem
+    _, _, V = np.linalg.svd(D, full_matrices=False)
+    coeffs = V[-1, :]
+    
+    return coeffs  # Returns the 10 coefficients of the fitted ellipsoid equation
 
-BUS=1
+def undistort(data, coeffs):
+    # Extract ellipsoid parameters
+    A, B, C, D, E, F, G, H, I, J = coeffs
+    
+    # Compute the offset (center of the ellipsoid)
+    M = np.array([[2*A, D, E], [D, 2*B, F], [E, F, 2*C]])
+    center = -np.linalg.solve(M, np.array([G, H, I]))
+    
+    # Translate data to center the ellipsoid at the origin
+    translated_data = data - center
+    
+    # Compute the transformation matrix (rotation and scaling)
+    eigenvalues, eigenvectors = np.linalg.eig(M)
+    scale_factors = np.sqrt(1 / np.abs(eigenvalues))  # Scale factors to make the ellipsoid a unit sphere
+    
+    # Align axes by rotating data to the principal axes
+    aligned_data = (eigenvectors.T @ translated_data.T).T
+    
+    # Apply scaling correction
+    scaled_data = aligned_data * scale_factors
+    
+    # Rotate back to the original orientation
+    corrected_data = (eigenvectors @ scaled_data.T).T
+    
+    return corrected_data
 
-MPU_6050_I2C_ADDR=0x68
+ 
+ 
+if __name__ == '__main__':
+   NUM_POSES = 15
+   NUM_SAMPLES = 10000
 
-# Registers.
-MPU_6050_WHO_AM_I    = 0x75 # R/W
-MPU_6050_ACCEL_XOUT_H = 0x3B # R
-MPU_6050_ACCEL_XOUT_L = 0x3C # R
-MPU_6050_ACCEL_YOUT_H = 0x3D # R
-MPU_6050_ACCEL_YOUT_L = 0x3E # R
-MPU_6050_ACCEL_ZOUT_H = 0x3F # R
-MPU_6050_ACCEL_ZOUT_L = 0x40 # R
-MPU_6050_TEMP_OUT_H  = 0x41 # R
-MPU_6050_TEMP_OUT_L  = 0x42 # R
-MPU_6050_GYRO_XOUT_H = 0x43 # R
-MPU_6050_GYRO_XOUT_L = 0x44 # R
-MPU_6050_GYRO_YOUT_H = 0x45 # R
-MPU_6050_GYRO_YOUT_L = 0x46 # R
-MPU_6050_GYRO_ZOUT_H = 0x47 # R
-MPU_6050_GYRO_ZOUT_L = 0x48 # R
+   BUS=1
 
-pi=pigpio.pi() # open local Pi
+   MPU_6050_I2C_ADDR=0x68
 
-h = pi.i2c_open(BUS, MPU_6050_I2C_ADDR)
+   # Registers.
+   MPU_6050_WHO_AM_I    = 0x75 # R/W
+   MPU_6050_ACCEL_XOUT_H = 0x3B # R
+   MPU_6050_ACCEL_XOUT_L = 0x3C # R
+   MPU_6050_ACCEL_YOUT_H = 0x3D # R
+   MPU_6050_ACCEL_YOUT_L = 0x3E # R
+   MPU_6050_ACCEL_ZOUT_H = 0x3F # R
+   MPU_6050_ACCEL_ZOUT_L = 0x40 # R
+   MPU_6050_TEMP_OUT_H  = 0x41 # R
+   MPU_6050_TEMP_OUT_L  = 0x42 # R
+   MPU_6050_GYRO_XOUT_H = 0x43 # R
+   MPU_6050_GYRO_XOUT_L = 0x44 # R
+   MPU_6050_GYRO_YOUT_H = 0x45 # R
+   MPU_6050_GYRO_YOUT_L = 0x46 # R
+   MPU_6050_GYRO_ZOUT_H = 0x47 # R
+   MPU_6050_GYRO_ZOUT_L = 0x48 # R
 
-if h >= 0: # Connected OK?
-   accel_sum = np.array([0, 0, 0])
-   gyro_sum = np.array([0, 0, 0])
+   pi=pigpio.pi() # open local Pi
 
-   calibration_data = np.empty((num_samples, 6), dtype=np.int16)
+   h = pi.i2c_open(BUS, MPU_6050_I2C_ADDR)
 
-   # Initialise MPU_6050.
-   start_time = time.time()
-   print(f"Who Am I {pi.i2c_read_byte_data(h, MPU_6050_WHO_AM_I)}")
+   if h >= 0: # Connected OK?
+      accel_sum = np.array([0, 0, 0])
+      gyro_sum = np.array([0, 0, 0])
 
-   for i in tqdm(range(num_samples)):
-      # > = big endian
-      (s, b) = pi.i2c_read_i2c_block_data(h, MPU_6050_ACCEL_XOUT_H, 6 + 2 + 6)
+      calibration_data = np.empty((NUM_SAMPLES * NUM_POSES, 6), dtype=np.int16)
 
-      if s >= 0:
-         data = struct.unpack('>3hxx3h', b)
+      # Initialise MPU_6050.
+      start_time = time.time()
+      print(f"Who Am I {pi.i2c_read_byte_data(h, MPU_6050_WHO_AM_I)}")
 
-         calibration_data[i] = data
-         # print(f"Acceleration: {calibration_data[read, :3]}")
-         # print(f"Gyro: {calibration_data[read, 3:]}")
+      for n in range(NUM_POSES):
+         input(f"Press enter once the robot is stable...")
+         print(f"Recording...")
+         for i in tqdm(range(NUM_SAMPLES)):
+            (s, b) = pi.i2c_read_i2c_block_data(h, MPU_6050_ACCEL_XOUT_H, 6 + 2 + 6)
 
-   calib_mean = np.mean(calibration_data, axis=0)
-   print(calib_mean[:3])
-   print(calib_mean[3:])
+            if s >= 0:
+               # > = big endian
+               calibration_data[n*NUM_POSES + i] = struct.unpack('>3hxx3h', b)
 
-   np.save("imu_calibration_data", calibration_data)
-   print("Runtime:", time.time() - start_time)
+      calib_mean = np.mean(calibration_data, axis=0)
+      print(calib_mean[:3])
+      print(calib_mean[3:])
 
-   pi.i2c_close(h)
+      np.save("imu_calibration_data", calibration_data)
 
-pi.stop()
+   pi.stop()
 
